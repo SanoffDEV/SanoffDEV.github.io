9d3ed7aa29ae945eb28067a9300067f4b4c5bd5a
MaPicot <picotmathis42@gmail.com>
2024-03-05 21:46:30 +0100

    t

--- js/index.js
+++ js/index.js
@@ -6,27 +6,27 @@ const infoscontainer = document.querySelector(".infoscontainer");
 let initialBtnStyle = {
   left: btn.style.left,
   top: btn.style.top,
+  opacity: btn.style.opacity,
 };
 
 infoscontainer.addEventListener("mouseout", () => {
   setTimeout(() => {
     infosbtn.style.display = "block";
   }, 2500);
-
-  infoscontainer.style.transition = "transform 2s"; 
+  infoscontainer.style.transition = "transform 2s";
   infoscontainer.style.transform = "translateX(0)";
 });
 
 infosbtn.addEventListener("mouseover", () => {
   infosbtn.style.transition = "opacity 1s, transform 1s";
   infosbtn.style.display = "none";
-  infoscontainer.style.transition = "transform 1s"; 
+  infoscontainer.style.transition = "transform 1s";
   infoscontainer.style.transform = "translateX(-205px)";
 });
 
 infosbtn.addEventListener("mouseout", () => {
   infosbtn.style.display = "block";
-  infoscontainer.style.transition = "transform 1s"; 
+  infoscontainer.style.transition = "transform 1s";
   infoscontainer.style.transform = "translateX(0)";
 });
 
@@ -45,16 +45,4 @@ togglebtn.addEventListener("click", () => {
     btn.style.left = initialBtnStyle.left;
     btn.style.top = initialBtnStyle.top;
   }
-});
-
-btn2.addEventListener("click", () => {
-  ul.style.display = "block";
-});
-
-togglebtn.addEventListener("click", () => {
-  sidebar.classList.toggle("active");
-});
-
-headercontent.addEventListener("click", () => {
-  sidebar.classList.remove("active");
 });
