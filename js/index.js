ccf6b1cba17843039e3bb4e942a12847b7cc8330
MaPicot <picotmathis42@gmail.com>
2024-03-06 01:13:02 +0100

    yey

--- js/index.js
+++ js/index.js
@@ -1,39 +1,11 @@
 const sidebar = document.getElementById("side-bar");
 const headercontent = document.querySelector(".headercontent");
 const togglebtn = document.getElementById("btn");
-const infosbtn = document.querySelector(".infos");
-const infoscontainer = document.querySelector(".infoscontainer");
 let initialBtnStyle = {
   left: btn.style.left,
   top: btn.style.top,
   opacity: btn.style.opacity,
 };
-
-infoscontainer.addEventListener("mouseout", () => {
-  setTimeout(() => {
-    infosbtn.style.display = "block";
-  }, 2500);
-  infoscontainer.style.transition = "transform 2s";
-  infoscontainer.style.transform = "translateX(0)";
-});
-
-infosbtn.addEventListener("mouseover", () => {
-  infosbtn.style.transition = "opacity 1s, transform 1s";
-  infosbtn.style.display = "none";
-  infoscontainer.style.transition = "transform 1s";
-  infoscontainer.style.transform = "translateX(-205px)";
-});
-
-infosbtn.addEventListener("mouseout", () => {
-  infosbtn.style.display = "block";
-  infoscontainer.style.transition = "transform 1s";
-  infoscontainer.style.transform = "translateX(0)";
-});
-
-infoscontainer.addEventListener("mouseover", () => {
-  infoscontainer.style.transform = "translateX(-85px)";
-  infosbtn.style.display = "none";
-});
 
 togglebtn.addEventListener("click", () => {
   sidebar.classList.toggle("active");
